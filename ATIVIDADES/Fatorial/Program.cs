ca230fe372d12c163fb2c0c4c0f351c9ca808601
Giordan Luiz Cador <giordan.cador2015@gmail.com>
2025-08-20 21:25:39 -0300

    Recursividade

---
+++ ATIVIDADES/Fatorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int fact, num;
+        Console.Write("Digite um número para calcular o fatorial: ");
+
+        // take input from user 
+        num = Convert.ToInt32(Console.ReadLine());
+
+        Program obj = new Program();
+
+        // calling recursive function   
+        fact = obj.factorial(num);
+
+        Console.WriteLine("Fatorial de {0} é {1}", num, fact);
+    }
+
+    // recursive function 
+    public int factorial(int num)
+    {
+        // termination condition
+        if (num == 0)
+            return 1;
+        else
+            // recursive call
+            return num * factorial(num - 1);
+    }
+}
