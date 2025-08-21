15ae20e55ee82decf9ecbf9800093ba95ebbe468
Giordan Luiz Cador <giordan.cador2015@gmail.com>
2025-08-20 21:37:47 -0300

    ATIVIDADES

---
+++ ATIVIDADES/Recursividade/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter the lenght of the Fibonacci Series: ");
+int Length = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < Length; i++)
+{
+    Console.Write(" {0} ", FibonacciSeries(i));
+}
+Console.ReadKey();
+
+static int FibonacciSeries(int n)
+{
+    if (n == 0) return 0; // to return the first fibonacci number
+    if (n == 1) return 1; // to return the second number 
+    return FibonacciSeries (n - 1) + FibonacciSeries(n - 2);
+}
